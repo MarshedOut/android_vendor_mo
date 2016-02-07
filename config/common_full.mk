4cad6fbad5340c138560c0a65017167e7a4e641a
Tyler Dunn <robotdude@att.net>
2016-02-07 13:21:40 -0500

    config: Remove unused command-line tools

--- config/common_full.mk
+++ config/common_full.mk
@@ -6,28 +6,8 @@ include vendor/cm/config/cm_audio.mk
 
 # Optional CM packages
 PRODUCT_PACKAGES += \
-    Galaxy4 \
-    HoloSpiralWallpaper \
     LiveWallpapers \
     LiveWallpapersPicker \
-    MagicSmokeWallpapers \
-    NoiseField \
     PhaseBeam \
-    PhotoTable \
     SoundRecorder \
-    PhotoPhase \
     Screencast
-
-# Extra tools in CM
-PRODUCT_PACKAGES += \
-    7z \
-    lib7z \
-    bash \
-    bzip2 \
-    curl \
-    powertop \
-    unrar \
-    unzip \
-    vim \
-    wget \
-    zip
